ab062a9449361efd26f3c65bce8ab3725c4af524
sajjad-njr <maxsajjad29@gmail.com>
2022-07-01 22:40:03 +0600

    2-gretings with Name

---
+++ 2.First-Java/Assignment/src/Name.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Name :  ");
+        String name = input.next();
+        System.out.println("Gretings "+name);
+    }
+}
