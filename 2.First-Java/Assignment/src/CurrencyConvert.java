b52bb3841c7092d1cacc2e477ccf8fd1999aa7a8
sajjad-njr <maxsajjad29@gmail.com>
2022-07-03 21:10:13 +0600

    CurrencyConvert - 6

---
+++ 2.First-Java/Assignment/src/CurrencyConvert.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CurrencyConvert {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double taka = input.nextDouble();
+        System.out.println(taka / 87.60);
+    }
+}
