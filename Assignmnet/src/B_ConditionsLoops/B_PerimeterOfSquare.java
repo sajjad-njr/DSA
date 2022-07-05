6c2127ef382194383a950e82210eb1320e9991d0
sajjad-njr <maxsajjad29@gmail.com>
2022-07-05 22:30:28 +0600

    Perimeter Of Square

---
+++ Assignmnet/src/B_ConditionsLoops/B_PerimeterOfSquare.java
@@ -0,0 +1,13 @@
+package B_ConditionsLoops;
+
+import java.util.Scanner;
+
+public class B_PerimeterOfSquare {
+    //Perimeter Of Square = 4 * a
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double  a ;
+        a = input.nextDouble();
+        System.out.println(4*a);
+    }
+}
