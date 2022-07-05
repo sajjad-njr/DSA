2f7a0aa9496d85f5b51c0c9516cf765eba8c22e1
sajjad-njr <maxsajjad29@gmail.com>
2022-07-05 16:26:13 +0600

    Area Of Isosceles Triangle

---
+++ Assignmnet/src/B_ConditionsLoops/AreaOfIsoscelesTriangle.java
@@ -0,0 +1,18 @@
+package B_ConditionsLoops;
+
+import java.util.Scanner;
+
+public class AreaOfIsoscelesTriangle {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // area of Isosceles Triangle  = ( base * height ) / 2
+
+        double ans , base = input.nextDouble() , height = input.nextDouble();
+
+        ans = (base * height)/ 2;
+
+        System.out.println(ans);
+    }
+}
