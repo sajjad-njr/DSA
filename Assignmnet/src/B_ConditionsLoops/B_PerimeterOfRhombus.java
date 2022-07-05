9bc64ca77a4ab74a9b026ff1ba0dfafec5d02109
sajjad-njr <maxsajjad29@gmail.com>
2022-07-05 22:32:15 +0600

    Perimeter Of Rhombus

---
+++ Assignmnet/src/B_ConditionsLoops/B_PerimeterOfRhombus.java
@@ -0,0 +1,14 @@
+package B_ConditionsLoops;
+
+import java.util.Scanner;
+
+public class B_PerimeterOfRhombus {
+
+    //Perimeter Of Rhombus = 4 * a
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double  a ;
+        a = input.nextDouble();
+        System.out.println(4*a);
+    }
+}
